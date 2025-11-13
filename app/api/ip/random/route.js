55520ff8bd4105a5508529c2ba95bc1679171399
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-13 20:03:11 +0600

    Update route.js

--- app/api/ip/random/route.js
+++ app/api/ip/random/route.js
@@ -1,5 +1,10 @@
-import { supabase } from '@/lib/supabase'
+import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
+
+const supabaseUrl = 'https://bsvnmqoohwsdijqbzpzx.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzdm5tcW9vaHdzZGlqcWJ6cHp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwMzg2NzYsImV4cCI6MjA3ODYxNDY3Nn0.nyKCoTL5sj2A7AcEGbFJmZIMF2N09A-ubu0k8ZKrJLw'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 function generateRandomIP() {
   // Generate valid IP addresses
@@ -84,4 +89,4 @@ export async function GET(request) {
 
 export async function POST(request) {
   return GET(request)
-      }
+}
