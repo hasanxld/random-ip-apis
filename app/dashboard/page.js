512b1f6b237eab7420f51bc63f513c1d1f65cce3
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-13 20:01:02 +0600

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 export default function Dashboard() {
   const [apiKey, setApiKey] = useState('')
@@ -118,4 +118,4 @@ export default function Dashboard() {
       <Footer />
     </div>
   )
-                    }
+}
